870fba102a0d17dc8b507bf9a76e1aa034ef43b0
Shakya Peiris <shakyaimanjith32@gmail.com>
2021-06-06 18:23:24 +0530

    Update Button.js

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -4,10 +4,10 @@ import './button.css'
 const Button = (props) => {
 
     return(
-        <button className = "button"
-        disabled = {props.disabled}
-        onClick = {props.onClick}
-        type = {
+        <button className="button"
+        disabled={props.disabled}
+        onClick={props.onClick}
+        type={
             props.type || "button"
         }>{props.children}</button>
     )
